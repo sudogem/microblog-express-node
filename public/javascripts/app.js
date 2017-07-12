107413c823dcfbda67a6c1db6db05ed6d5e10e31
Arman Ortega <ortegaaa@ph.ibm.com>
2017-07-12 18:58:00 +0800

    Upgrade from Angularjs v1.3.15 to AngularJS v1.4.14

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,15 +3,22 @@
 var app = angular.module('blog', [
   'blog.controllers',
   'flash.services',
-  'ngRoute'
+  'ngRoute',
+  'ngCookies',
 ]);
 
-app.config(['$routeProvider', function($routeProvider) {
+  app.run(['$rootScope', '$window', '$cookies', '$location', function($rootScope, $window, $cookies, $location) {
+    var currentUser = $cookies.getObject('user');
+    if (currentUser) {
+      $rootScope.user = currentUser;
+    }
+    console.log('c=', $location);
+  }])
+  .config(['$routeProvider', function($routeProvider) {
   $routeProvider.
     when('/', { templateUrl: 'partials/index', controller: 'IndexController' }).
     when('/add', { templateUrl: 'partials/add_new_post', controller: 'AddNewPostController' }).
     when('/edit_post/:id', { templateUrl: 'partials/edit_post', controller: 'EditPostController' }).
     when('/delete_post/:id', { templateUrl: 'partials/delete_post', controller: 'DeletePostController' }).
     otherwise({ redirectTo: '/' });
-
   }]);
